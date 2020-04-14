1738cb97e54d89a23052a5bd9c39a8d05bbc8243
Ehsan Mortazavi <e.mortazavi82@gmail.com>
2020-04-14 20:48:23 +0430

    Change code to dictionary type
    Change the simple code to recognize the vowel letters to the code counter by dictionary

--- P002.py
+++ P002.py
@@ -1,11 +1,15 @@
 while True:
     word = input("input word = ")
     v = ["a", "e", "i", "o", "u"]
-    found = []
+    found = {}
+    found['a'] = 0
+    found['e'] = 0
+    found['i'] = 0
+    found['o'] = 0
+    found['u'] = 0
+
     for letter in word:
         if letter in v:
-            if letter not in found:
-                found.append(letter)
-    for vowel in found:
-            print(vowel)
-        
+            found[letter] += 1
+    for k, v in found.items():
+        print(k, 'Was Found', v, 'times(s).')
