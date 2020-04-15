f437129bcf60c67caddf64fd961be6a8e25e0717
Ehsan Mortazavi <e.mortazavi82@gmail.com>
2020-04-15 20:46:04 +0430

    code optimization
    use setdefualt syntax

--- P002.py
+++ P002.py
@@ -1,34 +1,11 @@
-<<<<<<< HEAD
 while True:
     word = input("input word = ")
     v = ["a", "e", "i", "o", "u"]
     found = {}
-    found['a'] = 0
-    found['e'] = 0
-    found['i'] = 0
-    found['o'] = 0
-    found['u'] = 0
-
+    
     for letter in word:
         if letter in v:
+            found.setdefault(letter, 0)
             found[letter] += 1
     for k, v in found.items():
-        if v > 0:
             print(k, 'Was Found', v, 'times(s).')
-=======
-while True:
-    word = input("input word = ")
-    v = ["a", "e", "i", "o", "u"]
-    found = {}
-    found['a'] = 0
-    found['e'] = 0
-    found['i'] = 0
-    found['o'] = 0
-    found['u'] = 0
-
-    for letter in word:
-        if letter in v:
-            found[letter] += 1
-    for k, v in found.items():
-        print(k, 'Was Found', v, 'times(s).')
->>>>>>> 5c7c21ebbe23dc232343e5fd420bd8e427113926
