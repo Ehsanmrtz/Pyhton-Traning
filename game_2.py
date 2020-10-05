fd7a6cc1844cc7c410db061c300d0114e4073f93
Ehsanmrtz <e.mortazavi82@gmail.com>
2020-10-05 19:21:09 +0330

    creat
    This is a simple numerical game

---
+++ game_2.py
@@ -0,0 +1,16 @@
+import random
+pc_n = random.randint(0,10)
+print ('Computer Sugguestion is :',pc_n)
+user_answer = input('Choose one of the answers(Yes, Smaller, Bigger):')
+while user_answer != 'Yes':
+    if user_answer == 'Bigger':
+        pc_n = random.randint(pc_n,10)
+        print ('Computer Sugguestion is :',pc_n)
+        user_answer = input('Choose one of the answers(Yes, Smaller, Bigger):')
+    else:
+        pc_n = random.randint(0,pc_n)
+        print ('Computer Sugguestion is :',pc_n)
+        user_answer = input('Choose one of the answers(Yes, Smaller, Bigger):')
+if user_answer == 'Yes':
+        print ('Yes, That is the number')
+            
